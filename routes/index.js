6c0a25ec012ce6460ede6ce42cd6fdb50d7a849c
Alf <alf.g.jr@gmail.com>
2013-02-24 14:32:47 -0800

    Weekend project.

---
+++ routes/index.js
@@ -0,0 +1,39 @@
+
+/*
+ * GET home page.
+ */
+
+var crypto = require('crypto'),
+    uuid = require('node-uuid'),
+    config = require('../config');
+
+
+exports.index = function(req, res){
+  var policy = JSON.stringify({
+                "expiration": config.expire_date,
+                  "conditions": [ 
+                    {"bucket": config.aws_bucket}, 
+                    ["starts-with", "$key", config.bucket_dir],
+                    {"acl": "public-read"},
+                    {"success_action_status": "201"},
+                  ]
+                });
+
+  // Create base64 policy and signature
+  var base64policy = new Buffer(policy).toString('base64');
+  var signature = crypto.createHmac('sha1', config.aws_secret).update(base64policy).digest('base64');
+  var file_key = uuid.v4(); // Generate uuid for filename
+
+  res.render('index', { aws_key: config.aws_key,
+                        policy: base64policy,
+                        signature: signature,
+                        redirect_host: config.redirect_host,
+                        file_key: file_key,
+                        host: config.host,
+                        bucket_dir: config.bucket_dir });
+};
+
+exports.share = function(req, res){
+    res.render('share', { aws_bucket: config.aws_bucket,
+                          bucket_dir: config.bucket_dir });
+}
