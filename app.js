3ec9d683402e8974f29e28ae82280732b5013f28
Alf <alf.g.jr@gmail.com>
2013-02-24 20:02:30 -0800

    Switched to jquery.fileupload

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/share', routes.share);
+app.get('/signed_urls', routes.signed_urls);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -37,3 +37,26 @@ exports.share = function(req, res){
     res.render('share', { aws_bucket: config.aws_bucket,
                           bucket_dir: config.bucket_dir });
 }
+
+exports.signed_urls = function(req, res){
+  var policy = JSON.stringify({
+                "expiration": config.expire_date,
+                  "conditions": [ 
+                    {"bucket": config.aws_bucket}, 
+                    {"acl": "public-read"},
+                    ["starts-with", "$key", config.bucket_dir],
+                    {"success_action_status": "201"},
+                  ]
+                });
+
+  // Create base64 policy and signature
+  var base64policy = new Buffer(policy).toString('base64');
+  var signature = crypto.createHmac('sha1', config.aws_secret).update(base64policy).digest('base64');
+  var file_key = uuid.v4(); // Generate uuid for filename
+
+    res.json({ policy: base64policy,
+               signature: signature,
+               key: 'test',
+               success_action_redirect: "/"
+            })
+}
