558c13ad61cfd71e618dcb289797692762be3499
Alf <alf.g.jr@gmail.com>
2018-06-29 14:33:14 -0700

    Add cors.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express'),
   routes = require('./routes'),
   http = require('http'),
+  cors = require('cors'),
   path = require('path'),
   logger = require('morgan'),
   config = require('./config');
@@ -11,6 +12,7 @@ app.set('port', process.env.PORT || config.port);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'hjs');
 app.use(logger('dev'));
+app.use(cors());
 app.use(require('less-middleware')(path.join(__dirname + '/public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
